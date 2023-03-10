e6ec6408f65c38641eece1bd7392999198fd3d36
Darron Park <darron1217@gmail.com>
2023-03-10 16:08:50 +0900

    feat: implement delegation, income summaries

--- app/services/app/income.go
+++ app/services/app/income.go
@@ -1,8 +1,9 @@
 package app
 
 import (
-	"github.com/rs/zerolog/log"
 	database "validator-dashboard/app/db"
+
+	"github.com/rs/zerolog/log"
 )
 
 func IncomeHistories(chain string) []database.IncomeHistory {
@@ -27,4 +28,40 @@ func IncomeHistories(chain string) []database.IncomeHistory {
 	}
 
 	return incomeHistory
+}
+
+func IncomeSummaryByDate(date string) database.IncomeSummary {
+	db := database.GetDB()
+
+	var incomeSummary database.IncomeSummary
+
+	query := `
+		SELECT 
+			SUM(i.reward * t.price) as total_reward_value,
+			SUM(i.commission * t.price) as total_commission_value,
+			SUM(g.reward_sum * t.price) as total_grant_reward_value
+		FROM income_history i
+			JOIN token_price t
+			ON 
+				i.chain = t.chain
+				AND
+				DATE(i.create_dt) = DATE(t.create_dt) + INTERVAL '1 days'
+			LEFT JOIN (
+				SELECT SUM(reward) reward_sum, chain
+				FROM grant_reward_history
+				WHERE DATE(create_dt) = $1
+				GROUP BY chain
+			) g
+			ON 
+				i.chain = g.chain
+		WHERE DATE(i.create_dt) = $1
+		GROUP BY DATE(i.create_dt)
+	`
+
+	err := db.Get(&incomeSummary, query, date)
+	if err != nil {
+		log.Err(err).Msg("failed to get income summary")
+	}
+
+	return incomeSummary
 }
